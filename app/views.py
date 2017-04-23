c32ca832674ea8f93c9d896dbb7353ff4dd99c58
Maksym Prysiazhnyi <maksym.prysiazhnyi@edgedesign.cz>
2017-04-23 11:35:47 +0200

    Song detail added. Song list fixed

--- app/views.py
+++ app/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from django.http import HttpResponse
 from django.shortcuts import render
 
@@ -8,3 +9,11 @@ def index(request):
     all_songs_list = Song.objects.order_by('-created_at')[:5]
     context = {'all_songs_list': all_songs_list}
     return render(request, 'app/index.html', context)
+
+
+def detail(request, song_id):
+    try:
+        song = Song.objects.get(pk=song_id)
+    except Song.DoesNotExist:
+        raise Http404("Song does not exist")
+    return render(request, 'app/detail.html', {'song': song})
